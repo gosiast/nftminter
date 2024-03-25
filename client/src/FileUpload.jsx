9a0b9cfe64ec47056751fb1afe1846d7437f9ccf
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-03-25 12:09:27 +0100

    created a component and displayed the css

---
+++ client/src/FileUpload.jsx
@@ -0,0 +1,14 @@
+const FileUpload = () => {
+	return (
+		<>
+			<div className="form">
+				<form>
+					<input type="file" className="choose" />
+					<button className="btn">NFT Minter</button>
+				</form>
+			</div>
+		</>
+	);
+};
+
+export default FileUpload;
